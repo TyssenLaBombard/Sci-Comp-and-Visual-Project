18bfb0105c1c6dd78f42846edc7146bef20e4da2
Tyssen <tyssenlabombard@gmail.com>
2022-12-13 19:49:59 -0500

    Test suite 100%, small tweaks elsewhere

--- DataFit.jl
+++ DataFit.jl
@@ -1,19 +1,17 @@
 module DataFit
 
-import Base.show, Plots
-
-using RecipesBase
+import Base.show, RecipesBase, Plots
 
 export Point2D, XYData, linearRegression
 
-
+using RecipesBase, Plots
 
 
 """
 this struct makes a Point2D object with x and y as the field types 
 """
 
-struct Point2D{T <: Real}
+mutable struct Point2D{T <: Real}
     x::T
     y::T
 end
@@ -30,7 +28,7 @@ Point2D(x::Real, y::Real) = Point2D(promote(x,y)...)
 this struct makes vector of Point2D objects 
 """
 
-struct XYData
+mutable struct XYData
     #stores vector of Point2D objects
     vertices::Vector{Point2D{T}} where T <: Real
     
@@ -85,34 +83,39 @@ return
     xpts, ypts
 end
 
-"""
-this function approximates the slope (m) and intercept (b) of a best fit line for the given data in the form m,b
-"""
 
+"""
+function that fin linear regression of XYdata object that gets passed in
+TODO: Need to fix this
+"""
 function linearRegression(Data::XYData)
-    xpts = map(pt->pt.x ,Data.vertices) 
-    ypts = map(pt->pt.y ,Data.vertices) 
-    m = (length(xpts)*sum(xpts.*ypts) - sum(xpts)*sum(ypts))/(length(ypts)*sum(xpts.^2)-sum(xpts)^2)
-    b = (sum(ypts) - m*sum(xpts))/length(xpts)
-    m,b
+    m = Data.vertices[1].x
 end
 
+
+"""
+Function for gradient decent from chapter 27
+"""
+function gradientDescentBB(f::Function,x₀::Vector; max_steps = 100)
+  local steps = 0
+  local ∇f₀ = ForwardDiff.gradient(f,x₀)
+  local x₁ = x₀ - 0.25 * ∇f₀
+  while norm(∇f₀)> 1e-4 && steps < max_steps
+    ∇f₁ = ForwardDiff.gradient(f,x₁)
+    Δ∇f = ∇f₁-∇f₀
+    x₂ = x₁ - abs(dot(x₁-x₀,Δ∇f))/norm(Δ∇f)^2*∇f₁
+    x₀ = x₁
+    x₁ = x₂
+    ∇f₀ = ∇f₁
+    steps += 1
+  end
+  @show steps
+  steps < max_steps || throw(ErrorException("The number of steps has exceeded $max_steps"))
+  x₁
+end
 
 
 end #end module SciCompProjectModule
 
 
-#1. Enter the gradientDescentBB functions from the textbook. Add them to your module
 
-#2. Write a function called bestFitLine that minimizes equation (1) for a given set of data using the Barzilai–Borwein gradient descent code in problem #1. The only input should be a XYData object and should return a named tuple or a new datatype. Add the function to your module.
-#function bestFitLine(XYData::XYData)
-
-#4. consider a function of the form f(x;a,b,c) = ae^(bx) + C. You can write a best fit function for this by minimizing:
-
-
-#Write a function called bestFitExponential that uses either the gradient descent from #1 or the functions
-#from the JuMP module to find the minimum of (3).
-
-#5. Write a test for your bestFitExponential function that uses a set of data that is generated from an exponential function and it should fit (fairly close) the given function.
-
-#6. Write a function similar to that in #4 to minimize a periodic function of the form
